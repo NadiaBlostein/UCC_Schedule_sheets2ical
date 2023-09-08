b006b6476f550760878980e54fb4ae5116d02f2a
NadiaBlostein <nadia.blostein@mail.mcgill.ca>
2023-09-08 13:53:29 +0100

    Debugging & restructuring

--- excel_to_ical.py
+++ excel_to_ical.py
@@ -1,25 +1,13 @@
-'''
-Quick script that allows user to download UCC graduate entry medicine (GEM) schedule into excel spreadsheet to convert it into icalendar event (instead of needing to do this manually).
-Relevant documentation:
-    * https://icalendar.readthedocs.io/en/latest/usage.html
-    * https://learnpython.com/blog/working-with-icalendar-with-python/
-'''
-
-from icalendar import Calendar, Event
+from icalendar import Calendar, Event, vCalAddress, vText
 from datetime import datetime
+from pathlib import Path
+import os
+import pytz
 import pandas as pd
 import openpyxl
+import numpy as np
 
-excel_file_name = 'GEM1 Semester 1 2023 2024.xlsx'
-
-# Initiate the calendar
-cal = Calendar()
-
-# Some properties are required to be compliant
-cal.add('prodid', '-//My calendar product//example.com//')
-cal.add('version', '2.0')
-
-# Function that creates dictionary out of specific worksheet
+# =========== Get dictionary with all of the events
 def get_ical(excel_file_name, worksheet):
 
     # Read dataframe
@@ -27,46 +15,74 @@ def get_ical(excel_file_name, worksheet):
     except FileNotFoundError:
         print(f"Worksheet '{worksheet}' not found in the Excel file {excel_file}.")
         exit(1)
+    df = df.iloc[:, :6]
     df = df.reset_index(drop=True)
     df.columns = df.iloc[0]
     df = df[1:].reset_index(drop=True)
     df = df.drop(0).reset_index(drop=True)
     df.set_index(df.columns[0], inplace=True)
-    df.fillna(0, inplace=True)
     
     # Dictionary with all the relevant information
     events = []
     for column_name, column_data in df.items():
-        date = column_name #print(date.split()[0])
-        for i, value in enumerate(column_data):
-            if value != 0:
-                event = {}
-                event['summary'] = value
-
-                dtstart = f"{date.year}-{date.month}-{date.day} {df.index.tolist()[i].split('-')[0].rstrip()}"
-                dtstart = datetime.strptime(dtstart, "%Y-%m-%d %H:%M")
-                event['dtstart'] = dtstart
-
-                dtend = f"{date.year}-{date.month}-{date.day} {df.index.tolist()[i].split('-')[1].rstrip()}"
-                dtend = datetime.strptime(dtend, "%Y-%m-%d %H:%M")
-                event['dtend'] = dtend
-
-                events.append(event)
+        date = column_name
+        if isinstance(date, datetime):
+            for i, value in enumerate(column_data):
+                if value != 0 and isinstance(value,str):
+                    if 'lunch' not in value.lower():
+                        event = {}
+                        
+                        # Parse strings to make it cleaner
+                        value = value.replace('BHSC ','BHSC_')
+                        value = value.replace('GM1001','')
+                        value = value.replace('PHARMACOLOGY','Pharmacology')
+                        value = value.replace('BIOCHEMISTRY','Biochemistry')
+                        value = value.replace('ANATOMY','Anatomy')
+                        value = value.replace('PATHOLOGY /MEDMICRO','Pathology/Micro')
+                        value_list = value.split()
+                        
+                        # Separate location
+                        filtered_value_list = [word for word in value_list if 'BHSC_' not in word]
+                        location = [word for word in value_list if 'BHSC_' in word]
+                        output_value = ' '.join(filtered_value_list)
+                        
+                        event['summary'] = output_value
+                        dtstart = f"{date.year}-{date.month}-{date.day} {df.index.tolist()[i].split('-')[0].rstrip()}"
+                        dtstart = datetime.strptime(dtstart, "%Y-%m-%d %H:%M")
+                        event['dtstart'] = dtstart
+                        dtend = f"{date.year}-{date.month}-{date.day} {df.index.tolist()[i].split('-')[1].rstrip()}"
+                        dtend = datetime.strptime(dtend, "%Y-%m-%d %H:%M")
+                        event['dtend'] = dtend
+                        if len(location) == 1: event['location'] = location[0]
+                        events.append(event)
     return(events)
 
+# =========== File name and sheet list
+file_name = 'GEM1 Semester 1 2023 2024'
+excel_file_name = file_name + '.xlsx'
 sheet_list = openpyxl.load_workbook(excel_file_name).sheetnames
 
-for i in range(5): # for worksheet in sheet_list:
+# =========== Initiate the calendar
+cal = Calendar()
+
+# =========== Some properties are required to be compliant
+cal.add('prodid', '-//My calendar product//example.com//')
+cal.add('version', '2.0')
+
+# =========== Populate calendar with events!
+for i in range(14):
     event_dict = get_ical(excel_file_name, worksheet = sheet_list[i])
-    # Add subcomponents
+
     for course in event_dict:
         event = Event()
         event.add('summary', course['summary'])
         event.add('dtstart', course['dtstart'])
         event.add('dtend', course['dtend'])
-        #event.add('location', course['Location'])
+        if course.get('location') is not None:
+            event.add('location', course['location'])
         cal.add_component(event)
 
-f = open('GEM1_Semester_1_2023_2024.ics', 'wb')
+# =========== Write icalendar file
+f = open(file_name + '.ics', 'wb')
 f.write(cal.to_ical())
 f.close()
